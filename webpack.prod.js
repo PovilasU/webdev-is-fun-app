892e07a433f186c3dcaf6576d323da0cd9456057
povilasurbonas <povilas.urbonas85@gmail.com>
2020-03-16 06:39:55 +0100

    webpack optimization installed gzip, brotli, also created local server for testing

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin"); //gzip
+const BrotliPlugin = require("brotli-webpack-plugin"); //brotli
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: "production",
@@ -30,7 +34,22 @@ module.exports = merge(common, {
         removeStyleLinkTypeAttributes: true,
         useShortDoctype: true
       }
+    }),
+    new CompressionPlugin({
+      filename: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 8192,
+      minRatio: 0.8
+    }),
+    new BrotliPlugin({
+      //brotli plugin
+      asset: "[path].br[query]",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8
     })
+    // new BundleAnalyzerPlugin()
   ],
   module: {
     rules: [
